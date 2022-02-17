127ed4e574cba27fed30b8021cacdcc46ef6f3be
Dilaksan <dilaksanraj510@gmail.com>
2022-02-17 22:09:42 +0100

    port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const PORT = process.env.PORT || 8080
 app.use(express.static(__dirname + '/dist/heroku-deploy'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/heroku-deploy/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.set("port", PORT);
