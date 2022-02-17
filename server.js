ea40d1098708c85b8eff8811bbe0493640015e05
Dilaksan <dilaksanraj510@gmail.com>
2022-02-17 22:30:17 +0100

    port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const PORT = process.env.PORT || 5000
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 8080
+
 app.use(express.static(__dirname + '/dist/heroku-deploy'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/heroku-deploy/index.html'));});
-app.set("port", PORT);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
