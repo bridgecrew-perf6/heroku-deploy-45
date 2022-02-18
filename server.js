7539e929102d5ec027a0f9d560c547d5cc695143
Dilaksan <dilaksanraj510@gmail.com>
2022-02-18 09:35:01 +0100

    port

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.use(express.static(__dirname + '/dist/heroku-deploy'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/heroku-deploy/index.html'));});
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
